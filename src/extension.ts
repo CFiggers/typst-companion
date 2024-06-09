e29978e37b039ec87bf34d574ee3ad9fff1c40b6
Wenzhuo Liu <mgt@oi-wiki.org>
2024-06-09 16:25:20 +0800

    chore: add tinymist detection

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,10 @@ import * as formatting from './format';
 export function activate(context: vscode.ExtensionContext) {
 	
     const typstLSP = vscode.extensions.getExtension('nvarner.typst-lsp');
-    if(!typstLSP){
+    const tinymistLSP = vscode.extensions.getExtension('myriad-dreamin.tinymist');
+    if(!typstLSP && !tinymistLSP){
         void vscode.window.showWarningMessage(
-            'Typst Companion Says:\n\nHi there! This extension is designed to complement and accompany Nathan Varner\'s "Typst LSP" extension. It doesn\'t look like you have that extension installed!\n\nYou probably want to go install Typst LSP in addition to this one.',
+            'Typst Companion Says:\n\nHi there! This extension is designed to complement and accompany Nathan Varner\'s "Typst LSP" or Myriad Dreamin\'s "Tinymist Typst" extension. It doesn\'t look like you have that extension installed!\n\nYou probably want to go install Typst LSP or Tinymist Typst in addition to this one.',
             'Got it, thanks!'
         );
     }
